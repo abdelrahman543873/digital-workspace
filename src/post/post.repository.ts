a9086ada73471db06383108362442aa2aabaceb2
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2021-10-12 20:57:07 +0200

    feat: added repos

--- src/post/post.repository.ts
+++ src/post/post.repository.ts
@@ -3,12 +3,13 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Post, PostDocument } from './schema/post.schema';
 import { AddPostInput } from './inputs/add-post.input';
+import { BaseRepository } from '../shared/generics/repository.abstract';
 
 @Injectable()
-export class PostRepository {
-  constructor(
-    @InjectModel(Post.name) private postSchema: Model<PostDocument>,
-  ) {}
+export class PostRepository extends BaseRepository<Post> {
+  constructor(@InjectModel(Post.name) private postSchema: Model<PostDocument>) {
+    super(postSchema);
+  }
 
   async addPost(input: AddPostInput) {
     return await this.postSchema.create(input);
