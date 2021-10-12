824a93f1e6dfca00cf2159e9b13d1feb1e3629da
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2021-10-12 20:15:31 +0200

    feat: added logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'winston-mongodb';
+import { utilities, WinstonModule } from 'nest-winston';
+import { format, transports } from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new transports.MongoDB({
+          level: 'warn',
+          db: 'mongodb://localhost:27017/logs',
+          options: {
+            useUnifiedTopology: true,
+          },
+          name: 'mongoLog',
+        }),
+        new transports.Console({
+          level: 'info',
+          format: format.combine(
+            format.timestamp(),
+            format.ms(),
+            utilities.format.nestLike(),
+          ),
+        }),
+      ],
+    }),
+  });
   await app.listen(3000);
 }
 bootstrap();
