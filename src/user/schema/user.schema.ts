a1aeb39733bb77d03dd7cd3d89515b70c65d8b95
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-10-19 12:28:31 +0200

    feat: added select false to db

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, ObjectId } from 'mongoose';
-import { Exclude } from 'class-transformer';
 export type UserDocument = User & Document;
 
 @Schema({ versionKey: false, timestamps: true })
@@ -12,13 +11,8 @@ export class User {
   @Prop({ unique: true, required: true })
   email: string;
 
-  @Exclude()
-  @Prop({ unique: true, required: true })
+  @Prop({ unique: true, required: true, select: false })
   password: string;
-
-  constructor(partial: Partial<User>) {
-    Object.assign(this, partial);
-  }
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
