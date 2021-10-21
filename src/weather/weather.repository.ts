1f8959bdc947234f1f45778b8e9bb43ed5062cfa
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2021-10-21 11:22:43 +0200

    fix: weather date

--- src/weather/weather.repository.ts
+++ src/weather/weather.repository.ts
@@ -13,14 +13,14 @@ export class WeatherRepository {
         `/onecall?lat=${input.lat}&lon=${input.long}&exclude=current,minutely,hourly,alerts&appid=${process.env.WEATHER_API_KEY}&units=metric`,
       ),
     );
-    const date = new Date();
-    return response.data.daily.map((forecast) => {
+    return response.data.daily.map((forecast, index) => {
+      const date = new Date();
+      date.setDate(date.getDate() + index);
       const formattedForecast = {
-        date: date,
+        date,
         temperature: forecast.temp.max,
         windSpeed: forecast.wind_speed,
       };
-      date.setDate(date.getDate() + 1);
       return formattedForecast;
     });
   }
