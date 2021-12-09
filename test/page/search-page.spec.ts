425da727aa373eb1e7e5b972d7782343ebc13c17
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2021-12-09 06:19:50 +0200

    feat: added testing for create and search

--- test/page/search-page.spec.ts
+++ test/page/search-page.spec.ts
@@ -1,8 +1,9 @@
+import { buildPageParams } from './../../src/page/page.factory';
 import { testRequest } from '../request';
 import { HTTP_METHODS_ENUM } from '../request.methods.enum';
 import { userFactory } from '../../src/user/user.factory';
 import { pageFactory } from '../../src/page/page.factory';
-import { SEARCH_PAGES } from '../endpoints/page.endpoints';
+import { CREATE_PAGE, SEARCH_PAGES } from '../endpoints/page.endpoints';
 describe('search pages suite case', () => {
   it('should search pages by inexact', async () => {
     const user = await userFactory();
@@ -13,5 +14,24 @@ describe('search pages suite case', () => {
       token: user.token,
     });
     expect(res.body.docs.length).toBeGreaterThanOrEqual(1);
+  });
+
+  it('should create page and find it in search', async () => {
+    const user = await userFactory();
+    const page = await buildPageParams();
+    const res = await testRequest({
+      method: HTTP_METHODS_ENUM.POST,
+      url: CREATE_PAGE,
+      variables: {
+        name: page.name,
+      },
+      token: user.token,
+    });
+    const res1 = await testRequest({
+      method: HTTP_METHODS_ENUM.GET,
+      url: `${SEARCH_PAGES}?keyword=${res.body.name.substring(0, 2)}`,
+      token: user.token,
+    });
+    expect(res1.body.docs[0].name).toBe(res.body.name);
   });
 });
