4319d25fc4ddae7eb87cf9237fc5b14c4e8be510
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2022-01-18 15:40:33 +0200

    hotfix

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { readFileSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
 import { ENV_VARIABLE_NAMES } from '../../app.const';
 @Module({
   imports: [
@@ -10,9 +10,10 @@ import { ENV_VARIABLE_NAMES } from '../../app.const';
         // done this way to be able to connect in case of testing
         // docker and real runtime without docker
         uri:
-          configService.get<string>(ENV_VARIABLE_NAMES.MONGO_DB) ||
-          JSON.parse(readFileSync('globalConfig.json', 'utf-8')).mongoUri ||
-          configService.get<string>(ENV_VARIABLE_NAMES.LOCAL_MONGO_DB),
+          (existsSync('globalConfig.json') &&
+            JSON.parse(readFileSync('globalConfig.json', 'utf-8')).mongoUri) ||
+          configService.get<string>(ENV_VARIABLE_NAMES.LOCAL_MONGO_DB) ||
+          configService.get<string>(ENV_VARIABLE_NAMES.MONGO_DB),
         connectionFactory: (connection) => {
           // eslint-disable-next-line @typescript-eslint/no-var-requires
           connection.plugin(require('mongoose-aggregate-paginate-v2'));
