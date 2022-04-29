f413a14bb54b0a644d4ec5a8f3456f8e55e4233d
abdelrahamn543873 <abdelrahmanmysara33@gmail.com>
2022-04-29 17:11:12 +0200

    hotfix: direct manager validator

--- test/user/add-user.spec.ts
+++ test/user/add-user.spec.ts
@@ -4,10 +4,18 @@ import { HTTP_METHODS_ENUM } from '../request.methods.enum';
 import { ADD_USER } from '../endpoints/user.endpoints';
 import { skillFactory } from '../../src/skill/skill.factory';
 import { GENDER } from '../../src/app.const';
+import { countryFactory } from '../../src/country/country.factory';
+import { userFactory } from '../../src/user/user.factory';
+import { levelFactory } from '../../src/level/level.factory';
+import { EmploymentTypeFactory } from '../../src/employment-type/employment-type.factory';
 describe('register user suite case', () => {
   it('should register user and return a token', async () => {
     const params = await buildUserParams();
     const skill = await skillFactory();
+    const country = await countryFactory();
+    const user = await userFactory();
+    const level = await levelFactory();
+    const employmentType = await EmploymentTypeFactory();
     const res = await testRequest({
       method: HTTP_METHODS_ENUM.POST,
       url: ADD_USER,
@@ -19,6 +27,10 @@ describe('register user suite case', () => {
         phone: params.phone,
         skills: [skill._id.toString()],
         gender: GENDER[0],
+        country: country._id.toString(),
+        directManagerId: user._id.toString(),
+        level: level._id.toString(),
+        employmentType: employmentType._id.toString(),
       },
     });
     expect(res.body.skills[0]).toBe(skill._id.toString());
